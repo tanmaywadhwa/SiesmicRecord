835c142ce247cc06f0c954a6322fc65c94d6c735
Tanmay Wadhwa <wadhwa.tanmay@gmail.com>
2015-05-06 23:16:02 -0400

    Added a sensor calibration between main and recording activities.

--- src/com/tanmaywadhwa/seismicrecord/MainActivity.java
+++ src/com/tanmaywadhwa/seismicrecord/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startButton=(Button) findViewById(R.id.button1);
+        startButton=(Button) findViewById(R.id.button1); 
     }
     
 //    public void startRecording(View view){
